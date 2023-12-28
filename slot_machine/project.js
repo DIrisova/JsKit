21d794eac95811b33508256303c0fe71e9d67967
DIrisova <dilobar@j2health.com>
2023-12-27 23:15:02 -0500

    Step3: collect a bet amount

--- slot_machine/project.js
+++ slot_machine/project.js
@@ -34,7 +34,18 @@ const getNumberOfLines = () => {
   }
 };
 
-const depositAmount = deposit();
-console.log(`You have deposited ${depositAmount} dollars.`);
+const getBet = (balance, lines) => {
+    while (true) {
+        const bet = prompt("Enter the bet per line: ");
+        const numberBet = parseFloat(bet);
+        if (isNaN(numberBet) || numberBet <= 0 || numberBet > balance / lines) {
+            console.log("Invalid bet amount. Please try again.");
+        } else {
+            return numberBet;
+        }
+    }
+};
 
+let balance = deposit();
 const numberOfLines = getNumberOfLines();
+const bet = getBet(balance, numberOfLines);
