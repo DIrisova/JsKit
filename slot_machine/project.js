22ff7437bce282fdc830560c9587964839b6a663
DIrisova <dilobar@j2health.com>
2023-12-28 00:39:13 -0500

    STEP4: spin the slot machine

--- slot_machine/project.js
+++ slot_machine/project.js
@@ -9,29 +9,49 @@
 
 const prompt = require("prompt-sync")();
 
-const deposit = () => {
-  while (true) {
-    const depositAmont = prompt("Enter deposit amount: ");
-    const numberDepositAmount = parseFloat(depositAmont);
+const ROWS = 3;
+const COLS = 3;
 
-    if (isNaN(numberDepositAmount) || numberDepositAmount <= 0) {
-      console.log("Invalid deposit amount. Please try again.");
-    } else {
-      return numberDepositAmount;
+const SYMBOLS_COUNT = {
+    A: 2,
+    B: 4,
+    C: 6,
+    D: 8
+}    
+
+const SYMBOL_VALUE = {
+    A: 5,
+    B: 4,
+    C: 3,
+    D: 2
+}
+
+
+
+
+const deposit = () => {
+    while (true) {
+        const depositAmont = prompt("Enter deposit amount: ");
+        const numberDepositAmount = parseFloat(depositAmont);
+        
+        if (isNaN(numberDepositAmount) || numberDepositAmount <= 0) {
+            console.log("Invalid deposit amount. Please try again.");
+        } else {
+            return numberDepositAmount;
+        }
     }
-  }
 };
 
 const getNumberOfLines = () => {
-  while (true) {
-    const lines = prompt("Enter the number of lines to bet on (1-3): ");
-    const numberOfLines = parseFloat(lines);
-    if (isNaN(numberOfLines) || numberOfLines <= 0 || numberOfLines > 3) {
-      console.log("Invalid number of lines. Please try again.");
-    } else {
-      return numberOfLines;
+    while (true) {
+        const lines = prompt("Enter the number of lines to bet on (1-3): ");
+        const numberOfLines = parseFloat(lines);
+        if (isNaN(numberOfLines) || numberOfLines <= 0 || numberOfLines > 3) {
+            console.log("Invalid number of lines. Please try again.");
+        } else {
+            return numberOfLines;
+        }
     }
-  }
 };
 
 const getBet = (balance, lines) => {
@@ -45,6 +65,30 @@ const getBet = (balance, lines) => {
         }
     }
 };
+
+const spin = ()=> {
+    const symbols = [];
+    for (const [symbol, count] of Object.entries(SYMBOLS_COUNT)) {
+        for (let i = 0; i < count; i++) {
+            symbols.push(symbol);
+        }
+    }
+    const reels = [[], [], []];
+    for (let i = 0; i < COLS; i++) {
+        const reelSymbols = [...symbols];
+        for (let j = 0; j < ROWS; j++) {
+            const randomIndex = Math.floor(Math.random() * reelSymbols.length);
+            const selectSymbol = reelSymbols[randomIndex];
+            reels[i].push(selectSymbol);
+            reelSymbols.splice(randomIndex, 1);
+
+        }
+    }
+    return reels;
+};
+
+const reels = spin();
+console.log(reels);
 
 let balance = deposit();
 const numberOfLines = getNumberOfLines();
