1cc0d5b1ace69cec89843a0504270a87689eaefd
DIrisova <dilobar@j2health.com>
2023-12-27 23:05:05 -0500

    Step2: determen number of lines to bet

--- slot_machine/project.js
+++ slot_machine/project.js
@@ -1,6 +1,6 @@
 // STEPS:
 // 1. deposit some money
-// 2. determen number of lines to bet 
+// 2. determen number of lines to bet
 // 3. collect a bet amount
 // 4. spin the slot machine
 // 5. check if the slot machine has won
@@ -10,24 +10,31 @@
 const prompt = require("prompt-sync")();
 
 const deposit = () => {
-    while (true) {
-        const depositAmont = prompt("Enter deposit amount: ");
-            const numberDepositAmount = parseFloat(depositAmont);
+  while (true) {
+    const depositAmont = prompt("Enter deposit amount: ");
+    const numberDepositAmount = parseFloat(depositAmont);
 
-            if (isNaN(numberDepositAmount) || numberDepositAmount <= 0) {
-                console.log("Invalid deposit amount. Please try again.");
-            } else {
-                return numberDepositAmount;
-            }
-        }
+    if (isNaN(numberDepositAmount) || numberDepositAmount <= 0) {
+      console.log("Invalid deposit amount. Please try again.");
+    } else {
+      return numberDepositAmount;
+    }
+  }
+};
 
 const getNumberOfLines = () => {
-    
-}
-
-
-
+  while (true) {
+    const lines = prompt("Enter the number of lines to bet on (1-3): ");
+    const numberOfLines = parseFloat(lines);
+    if (isNaN(numberOfLines) || numberOfLines <= 0 || numberOfLines > 3) {
+      console.log("Invalid number of lines. Please try again.");
+    } else {
+      return numberOfLines;
+    }
+  }
 };
 
 const depositAmount = deposit();
 console.log(`You have deposited ${depositAmount} dollars.`);
+
+const numberOfLines = getNumberOfLines();
